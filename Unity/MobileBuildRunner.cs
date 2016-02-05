3296708fec933a9a2aaa9044e5010f88b8372b40
Ryan Kennedy <rkennedy@gravityjack.com>
2016-02-05 10:47:28 -0800

    fixed copy/paste error.

--- Unity/MobileBuildRunner.cs
+++ Unity/MobileBuildRunner.cs
@@ -50,12 +50,12 @@ public class MobileBuildRunner
 		}
 
 		string keyAliasName = ReadKeyAliasName ();
-		if (keystorePass != null) {
+		if (keyAliasName != null) {
 			PlayerSettings.Android.keyaliasName = keyAliasName;
 		}
 
 		string keyAliasPass = ReadKeyAliasPass ();
-		if (keystorePass != null) {
+		if (keyAliasPass != null) {
 			PlayerSettings.Android.keyaliasPass = keyAliasPass;
 		}
 
